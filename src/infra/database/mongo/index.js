d117786ecd99f32967c29af28704520b21952e39
Matheus <matheus.pgm2000@gmail.com>
2022-02-04 16:00:25 -0300

    feat: create connection database

---
+++ src/infra/database/mongo/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+class Database {
+	constructor () {
+		this.connect();
+	}
+
+	connect () {
+		return mongoose
+			.connect(process.env.DB_HOST)
+			.catch((error) => console.log(error));
+	}
+}
+
+module.exports = new Database().connect();
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const App = require('./app.js')
-require('dotenv').config()
-const port = process.ENV.PORT 
+const App = require('./app.js');
+require('dotenv').config();
+const port = process.ENV.PORT; 
 
 App.listen(port || 3000, () => {
-    console.log(`Server is running on port ${port}`)
-})
+	console.log(`Server is running on port ${port}`);
+});
