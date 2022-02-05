6db730bf9151bbf62787b5651c7eb7db25face53
Matheus <matheus.pgm2000@gmail.com>
2022-02-05 13:07:02 -0300

    feat: create route update

--- src/app/respository/ProjectRepository.js
+++ src/app/respository/ProjectRepository.js
@@ -5,6 +5,7 @@ class ProjectRepository {
 	async create (payload) {
 		return schema.create(payload);
 	}
+
 	async findAll(payload) {
 		const myCustomLabels = {
 			totalDocs: 'total',
@@ -25,9 +26,16 @@ class ProjectRepository {
 		};
 		return schema.paginate(payload,options,{});
 	}
+
 	async findById (id) {
 		return schema.findById(id);
 	}
+	
+	async update(id, payload) {
+		await schema.updateOne({ _id: id }, payload);
+		return schema.findOne({ _id: id });
+	}
+
 	async delete(id) {
 		return schema.deleteOne({ _id: id });
 	}		
